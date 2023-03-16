6fbf1d09c6842a16ba61253296717d9a8e8a1a62
Orina <dimaryorina@gmail.com>
2023-03-16 14:22:21 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C programme that prints a new line using puts functions
+ * Return: 0 (succeess)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return(0);
+} 
+
