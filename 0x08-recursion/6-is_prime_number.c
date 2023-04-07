efd7460d5700a3d38655a7baf297e1c73697c728
Orina <dimaryorina@gmail.com>
2023-04-07 01:17:07 -0700

    Inception. Is it possible

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * is_prime_number - determines number is prime
+ * @n: number to input
+ * Return: 1 if n is prime number 0 not prime number
+ */
+int real_prime(int n, int j);
+int is_prime_number(int n)
+
+{
+if (n <= 0)
+return (0);
+return (real_prime(n, n - 1));
+}
+
+/**
+ * real_prime - evaluates a prime number recussively
+ * @n: number to input
+ * @j: iterator
+ * Return: 1 if n is prime number 0 not prime number
+ */
+int real_prime(int n, int j)
+{
+if (n == 1)
+return (1);
+if (n % j == 0 && j > 0)
+return (0);
+return (real_prime(n, j - 1));
+}
