95b6a01bac515f3ae7fa66824da95bd319627f45
Orina <dimaryorina@gmail.com>
2023-04-11 03:16:49 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - allocates memory to new pointer
+ * @str: char
+ * Return: NULL
+ */
+char *_strdup(char *str)
+{
+	char *jjj;
+
+	int i, n = 0;
+
+	if (str == 0)
+		return (0);
+
+	i = 0;
+
+	while (str[i] != '\0')
+
+		i++;
+
+	jjj = malloc(sizeof(char) * (i + 1));
+
+	if (jjj == NULL)
+
+		return (NULL);
+
+	for (n = 0; str[n]; n++)
+
+		jjj[n] = str[n];
+	return (jjj);
+
+}
