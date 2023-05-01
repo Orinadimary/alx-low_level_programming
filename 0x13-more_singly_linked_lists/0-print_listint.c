1484e628528e7e0fb4f4771be26e8297236a2796
Orina <dimaryorina@gmail.com>
2023-05-01 03:17:15 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * print_listint - function that prints all the elements 
+ * print_listint - function that prints all the elements
  * @h: links lists to be printed
  * Return:  the number of nodes
  */
