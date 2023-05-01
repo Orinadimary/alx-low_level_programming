031a4b613266b7b83896eb7ac4c1f5ee40bd37b1
Orina <dimaryorina@gmail.com>
2023-05-01 04:16:00 -0700

    Find the loop

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint -  function that adds a new node at the beginning
+ * @head: pointer to list int
+ * @n: interger to oint to
+ * Return: new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
