94f9eab592836f013dc93c2368006045ba323ae7
Orina <dimaryorina@gmail.com>
2023-05-01 03:33:19 -0700

    List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,9 +4,9 @@
 #include "lists.h"
 
 /**
- * listint_len - function that returns the number of elements in a linked 
+ * listint_len - function that returns the number of elements in a linked
  * @h: pointer to the linked lists
- * @Return:  the number of nodes
+ * @Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
