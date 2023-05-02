949a4271c0feb7d5aace3ce6f0f05f1558762366
Orina <dimaryorina@gmail.com>
2023-05-02 05:12:15 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,7 +3,7 @@
  * _memset -  memory with a specific value
  * @s: address of memory
  * @b: the desired value
- * @n: number of bytes 
+ * @n: number of bytes
  * Return: array with new value for n bytes
  */
 char *_memset(char *s, char b, unsigned int n)
