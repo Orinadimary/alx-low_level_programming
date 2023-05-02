0b4ca8f9ae6443d511e4cacffcd31e98cb9b44b7
Orina <dimaryorina@gmail.com>
2023-05-02 03:34:40 -0700

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-int real_prime(int n, int j);
+int actual_prime(int n, int j);
 
 /**
 * is_prime_number - determines number is prime
@@ -13,20 +13,20 @@ int is_prime_number(int n)
 {
 if (n <= 1)
 return (0);
-return (real_prime(n, n - 1));
+return (actual_prime(n, n - 1));
 }
 
 /**
-* real_prime - evaluates a prime number recussively
+* actual_prime - evaluates a prime number recussively
 * @n: number to input
 * @j: iterator
 * Return: 1 if n is prime number 0 not prime number
 */
-int real_prime(int n, int j)
+int actual_prime(int n, int j)
 {
 if (n == 1)
 return (1);
 if (n % j == 0 && j > 0)
 return (0);
-return (real_prime(n, j - 1));
+return (actual_prime(n, j - 1));
 }
