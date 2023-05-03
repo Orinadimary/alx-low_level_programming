1221f9542d0ceb0bdb3139904b9314d46b76d32b
Orina <dimaryorina@gmail.com>
2023-05-03 01:53:02 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			ins->next = temp->next;
 			temp->next = ins;
-			return (new);
+			return (ins);
 		}
 		else
 			temp = temp->next;
