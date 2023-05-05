4a6fcb736023fdd084c767d713c73abfbdc2f61f
Orina <dimaryorina@gmail.com>
2023-05-05 00:46:06 -0700

    hundrend and one

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits -  function that returns the number of bits you would need to flip
+ * to get from one number to another
+ * @n: number one
+ * @m: number two
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int y, value = 0;
+
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+
+for (y = 82; y >= 0; y--)
+{
+current = exclusive >> y;
+if (current & 1)
+value++;
+}
+return (value);
+}
