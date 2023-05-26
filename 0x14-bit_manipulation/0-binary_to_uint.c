ec1c68d5396c77c77afb7c33d89a227e34adc80a
Orina <dimaryorina@gmail.com>
2023-05-26 07:05:04 -0700

    zero

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int x;
-
-	unsigned int dec_val = 0;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
-	for (x = 0; b[x]; x++)
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[x] < '0' || b[x] > '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		dec_val = 2 * dec_val + (b[x] - '0');
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (dec_val);
+
+	return (ui);
 }
