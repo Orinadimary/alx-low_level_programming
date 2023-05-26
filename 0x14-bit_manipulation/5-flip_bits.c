397c689b5744591afa056e946fcfa8e12579f3e3
Orina <dimaryorina@gmail.com>
2023-05-26 07:29:30 -0700

    hundrend and one

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int nbits;
+	unsigned int qbits;
 
-	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	for (qbits = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
-			nbits++;
+			qbits++;
 	}
 
-	return (nbits);
+	return (qbits);
 }
