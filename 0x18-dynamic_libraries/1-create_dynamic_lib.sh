214b62eccf1688e5f2cb9afdeb7cfa2db8718712
Orina <dimaryorina@gmail.com>
2023-06-19 14:01:33 -0700

    We have no past and no future

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wetra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=. :$LD_LIBRARY_PATH
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+gcc -shared -o liball.so ${c_files}
