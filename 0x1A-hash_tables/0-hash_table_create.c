09734ea69bd3ea1c133678acbccdd8b5fc1cf265
Orina <dimaryorina@gmail.com>
2023-07-06 13:29:01 -0700

    djb2

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -  function that creates a hash table.
+ * @size: size of the array
+ * Return: to the craeted hash tabble
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{       
+        hash_table_t *h;
+        unsigned long int n;
+
+        h = malloc(sizeof(hash_table_t));
+
+	if h == NULL
+		return (NULL);
+	h->size = size;
+	h->array = malloc(sizeof(hash_node_t*) * size;
+			if 
