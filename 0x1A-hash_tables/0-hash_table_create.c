8240a0f8878906b4f2884f6d531e4c379693325f
Orina <dimaryorina@gmail.com>
2023-07-06 23:10:05 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,4 +16,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	h->size = size;
 	h->array = malloc(sizeof(hash_node_t*) * size;
-			if 
+			if (h->array == NULL;
+				return (h);
