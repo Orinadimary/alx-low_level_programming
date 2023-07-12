0ecc9519b425f7111a427de011dcb555139a7474
Orina <dimaryorina@gmail.com>
2023-07-12 07:36:31 -0700

    0. >>> ht = {}

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,12 +4,13 @@
  * hash_table_get - function that retrieves a value associated with a key.
  * @ht: pointer to the hash table
  * @key: character from the pointer
- * 
+ *
  * Return: Null
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
+
 	unsighned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
@@ -19,6 +20,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 	node = ht->array[index];
-	while (node && strcmp(node->key, key) !=0)
-		node =node->next;
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 	return ((node == NULL) ? NULL : node->value);
